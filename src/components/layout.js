c6a7b604137754c12ed2ca4be6f4c7f5ec629943
Liyana <69780620+ffrixslee@users.noreply.github.com>
2021-08-12 16:59:47 +0800

    layout update

--- src/components/layout.js
+++ src/components/layout.js
@@ -3,6 +3,8 @@
  import Header from '../components/header'
  import Footer from '../components/footer'
 
+ import '../styles/index.css'
+
  const Layout = (props) => {
      return (
          <div>
